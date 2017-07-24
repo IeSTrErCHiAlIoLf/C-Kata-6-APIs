1c9483832b74a08ea6b528e4b11e7156a7d0f9a9
Carl Paulson <github.10.Carl_Paulson@spamgourmet.com>
2017-07-24 11:19:35 -0400

    Adding base C# project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Kata_6_APIs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
